30f9fc75ce775c58e400e33832c04b249982f9a5
Eraze86 <therese.rosvall@hotmail.se>
2022-01-30 13:25:28 +0100

    ćhange catpic to dogpic with a click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-bindings';
+  imgUrl: string = "https://s3.amazonaws.com/freecodecamp/relaxing-cat.jpg"
+  klassCat: string = "klassCat";
+  iLikeCats: boolean = false;
+  
+  dogImg(){
+    this.imgUrl = "https://images.dog.ceo/breeds/groenendael/n02105056_1061.jpg"
+  }
 }
